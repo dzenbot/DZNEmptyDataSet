7d1c3421b240083257b59031aee8be3bb99536e6
dzenbot <iromero@dzen.cl>
2014-06-07 13:45:26 -0400

    Adds color utility

---
+++ Examples/Applications/Applications/UIColor+Hexadecimal.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Hexadecimal.h
+//  Applications
+//
+//  Created by Ignacio on 6/7/14.
+//  Copyright (c) 2014 DZN Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Hexadecimal)
+
++ (UIColor *)colorWithHex:(NSString *)string;
+
+@end
---
+++ Examples/Applications/Applications/UIColor+Hexadecimal.m
@@ -0,0 +1,36 @@
+//
+//  UIColor+Hexadecimal.m
+//  Applications
+//
+//  Created by Ignacio on 6/7/14.
+//  Copyright (c) 2014 DZN Labs. All rights reserved.
+//
+
+#import "UIColor+Hexadecimal.h"
+
+@implementation UIColor (Hexadecimal)
+
++ (UIColor *)colorWithHex:(NSString *)string
+{
+    NSString *cleanString = [string stringByReplacingOccurrencesOfString:@"#" withString:@""];
+    if([cleanString length] == 3) {
+        cleanString = [NSString stringWithFormat:@"%@%@%@%@%@%@",
+                       [cleanString substringWithRange:NSMakeRange(0, 1)],[cleanString substringWithRange:NSMakeRange(0, 1)],
+                       [cleanString substringWithRange:NSMakeRange(1, 1)],[cleanString substringWithRange:NSMakeRange(1, 1)],
+                       [cleanString substringWithRange:NSMakeRange(2, 1)],[cleanString substringWithRange:NSMakeRange(2, 1)]];
+    }
+    if([cleanString length] == 6) {
+        cleanString = [cleanString stringByAppendingString:@"ff"];
+    }
+    
+    unsigned int baseValue;
+    [[NSScanner scannerWithString:cleanString] scanHexInt:&baseValue];
+    
+    float red = ((baseValue >> 24) & 0xFF)/255.0f;
+    float green = ((baseValue >> 16) & 0xFF)/255.0f;
+    float blue = ((baseValue >> 8) & 0xFF)/255.0f;
+    
+    return [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+}
+
+@end
