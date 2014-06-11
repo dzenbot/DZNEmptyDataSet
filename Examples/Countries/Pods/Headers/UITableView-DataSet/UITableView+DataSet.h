daa7c4577a5a793b2d1ace68fcb5f9be659b7b6a
dzenbot <iromero@dzen.cl>
2014-06-10 21:37:55 -0400

    Adds local pods files

---
+++ Examples/Countries/Pods/Headers/UITableView-DataSet/UITableView+DataSet.h
@@ -0,0 +1,1 @@
+../../../../../Source/UITableView+DataSet.h
---
+++ Examples/Countries/Pods/Pods-UITableView-DataSet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UITableView_DataSet : NSObject
+@end
+@implementation PodsDummy_Pods_UITableView_DataSet
+@end
