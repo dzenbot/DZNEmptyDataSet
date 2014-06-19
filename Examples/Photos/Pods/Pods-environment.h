a2d2f24684f86edc0e606268a76b1169d2581cc4
dzenbot <iromero@dzen.cl>
2014-06-19 13:14:16 -0400

    Adds the source as development pod to the new Colors sample application

---
+++ Examples/Photos/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// UITableView-DataSet
+#define COCOAPODS_POD_AVAILABLE_UITableView_DataSet
+#define COCOAPODS_VERSION_MAJOR_UITableView_DataSet 1
+#define COCOAPODS_VERSION_MINOR_UITableView_DataSet 1
+#define COCOAPODS_VERSION_PATCH_UITableView_DataSet 0
+
