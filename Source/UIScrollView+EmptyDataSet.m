1fc955e53284d744e17302e90a746117ef5dabb0
f33chobits <=>
2015-08-24 07:29:46 +0800

    Fix center zigzag
    Fix center zigzag

--- Source/UIScrollView+EmptyDataSet.m
+++ Source/UIScrollView+EmptyDataSet.m
@@ -819,6 +819,10 @@ NSString *dzn_implementationKey(id target, SEL selector)
     // When a custom offset is available, we adjust the vertical constraints' constants
     if (self.verticalOffset != 0 && self.constraints.count > 0) {
         centerYConstraint.constant = self.verticalOffset;
+        if ([self.superview respondsToSelector:@selector(contentInset)]) {
+            UIEdgeInsets inset = [[self.superview valueForKey:@"contentInset"] UIEdgeInsetsValue];
+            centerYConstraint.constant += inset.top;
+        }
     }
     
     // If applicable, set the custom view's constraints
