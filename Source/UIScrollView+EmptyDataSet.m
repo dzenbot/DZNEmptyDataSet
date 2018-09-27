46e31f271591b30e21ad6f7c2f3382ddd33bbf1c
Gareth Miller <ctingcter@gmail.com>
2018-09-27 13:14:45 +0100

    Used weakSelf in place of self->

--- Source/UIScrollView+EmptyDataSet.m
+++ Source/UIScrollView+EmptyDataSet.m
@@ -740,7 +740,8 @@ Class dzn_baseClassToSwizzleForTarget(id target)
     CGRect superviewBounds = self.superview.bounds;
     self.frame = CGRectMake(0.0, 0.0, CGRectGetWidth(superviewBounds), CGRectGetHeight(superviewBounds));
     
-    void(^fadeInBlock)(void) = ^{self->_contentView.alpha = 1.0;};
+    __weak __typeof(_contentView) weakContentView = _contentView;
+    void(^fadeInBlock)(void) = ^{weakContentView.alpha = 1.0;};
     
     if (self.fadeInOnDisplay) {
         [UIView animateWithDuration:0.25
