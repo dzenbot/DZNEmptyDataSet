14c625db55b1b8ae27eeaf865d0f5d6628356975
dzenbot <ignacio.romeroz@gmail.com>
2019-12-31 17:11:33 -0800

    Adding dummy project to test swift rewrite

---
+++ DZNEmptyDataSet/Sample - Bento/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bento
+//
+//  Created by Ignacio Romero Zurbuchen on 2019-12-31.
+//  Copyright © 2019 DZN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
