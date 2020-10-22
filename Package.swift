c1e94ec97ba52cc7efe9e0ac70230462ab30206e
Markus Mueller <mmlr@gmx.net>
2020-10-22 14:01:55 +0200

    Updates package manifest
    Signed-off-by: Markus Mueller <mmlr@gmx.net>

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // swift-tools-version:5.0
-
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
@@ -9,25 +9,21 @@ let package = Package(
         .tvOS(.v9)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DZNEmptyDataSet",
-            targets: ["DZNEmptyDataSet"]),
+            targets: ["DZNEmptyDataSet"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),        //
-
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DZNEmptyDataSet",
-            dependencies: [
-            ],
-            path: "Source",
-            exclude: ["DZNEmptyDataSet"]
+            dependencies: [],
+            path: ".",
+            sources: ["Source"],
+            publicHeadersPath: "Source"
         )
     ]
 )
